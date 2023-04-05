90d6fcc64e649b613926179220a4f5f2b63e3c95
MarcVer <marcvervoorn@hotmail.nl>
2023-04-05 13:50:27 +0200

    added documentation to job class

--- Job.h
+++ Job.h
@@ -24,20 +24,86 @@ public:
 	Job(unsigned long id);
 	virtual ~Job();
 
+	/**
+	* getEnd()
+	* @brief getter for endtime.
+	*/
 	unsigned long getEnd() const;
+	/**
+	* setEnd()
+	* @brief setter for endtime.
+	* @param a entime of current job
+	*/
 	void setEnd(unsigned long end);
+	/**
+	* isFinished()
+	* @brief returns true if finished, returns false if job is still running or not started.
+	*/
 	bool isFinished() const;
+	/**
+	* setFinished()
+	* @brief sets state of job
+	* @param true if job is finished, false if not
+	*/
 	void setFinished(bool finished);
+	/**
+	* getId()
+	* @brief returns id of job
+	*/
 	unsigned long getId() const;
+	/**
+	* getSlack()
+	* @brief returns slack of job
+	*/
 	unsigned long getSlack() const;
+	/**
+	* setSlack()
+	* @brief setter for slack of job
+	* @param slack
+	*/
 	void setSlack(unsigned long slack);
+	/**
+	* getStart()
+	* @brief returns starttime of job
+	*/
 	unsigned long getStart() const;
+	/**
+	* setStart()
+	* @brief sets starttime of job
+	* @param starttime to set.
+	*/
 	void setStart(unsigned long start);
+	/**
+	* isStarted()
+	* @brief returns true if job is started, returns false if not
+	*/
 	bool isStarted() const;
+	/**
+	* setStarted()
+	* @brief setter for startstatus
+	* @param true if job is started, false if not
+	*/
 	void setStarted(bool started);
+	/**
+	* getTasks()
+	* @brief returns all current Tasks
+	*/
 	std::queue<Task>& getTasks();
+	/**
+	* setTasks()
+	* @brief fills job with tasks
+	* @param ordered queue of tasks.
+	*/
 	void setTasks(const std::queue<Task> &tasks);
+	/**
+	* getTotalDurationOnStart()
+	* @brief returns total duration of all tasks in job.
+	*/
 	unsigned long getTotalDurationOnStart() const;
+	/**
+	* setTotalDurationOnStart()
+	* @brief sets totalDuration of all tasks in job.
+	*/
 	void setTotalDurationOnStart(unsigned long totalDurationOnStart);
 };
 
